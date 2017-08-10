442ac2e4f5919ad9383c86a3fe35797b995e1c85
binRick <rapidvps@gmail.com>
2017-08-10 17:40:44 +0000

    it works

--- index.js
+++ index.js
@@ -21,15 +21,11 @@ console.log('free -m : \n' + c.green(freeM));
 var msg = c.cyan.bgBlack('Current ZFS ARC Max is ') + c.white.bgBlack(prettyBytes(curMaxBytes));
 var promptMsg = 'Select new ARC Size: ';
 var unit = 'MB';
-var min = 256;
+var min = 0;
 var max = +os.totalmem() / 1024 / 1024;
-//var step = 128;
 var step = parseInt(max/8);
 
-console.log('max=', max);
-//process.exit();
 console.log(c.cyan.bgWhite(msg) + '\n');
-
 
 var curValue = parseInt(curMaxBytes / 1024 / 1024);
 curValue = parseInt(curValue / step) * step;
@@ -45,8 +41,8 @@ var rP = {
     step: step,
     unit: unit
 };
-console.log(pj.render(rP)+'\n\n');
+//console.log(pj.render(rP)+'\n\n');
 
 rangePrompt(promptMsg, rP)
-    .on('abort', (item) => console.log('You aborted, having chosen', item, 'kg'))
-    .on('submit', (item) => console.log('You chose', item, 'kg'))
+    .on('abort', (item) => console.log('You aborted, having chosen', item))
+    .on('submit', (item) => console.log('You chose', item))
