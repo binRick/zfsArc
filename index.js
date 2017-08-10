9ef7961ec331c974f7343bdcaf1d06c0c9600961
binRick <rapidvps@gmail.com>
2017-08-10 15:29:42 +0000

    initial ideas

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 #!/usr/bin/env node
 
 var prettyBytes = require('pretty-bytes'),
-    rangePrompt = require('range-prompt');
+    pj = require('prettyjson'),
+    rangePrompt = require('range-prompt'),
+    fs = require('fs'),
+    //maxArcFile = '/sys/module/zfs/parameters/zfs_arc_max',
+    maxArcFile = 'test',
+    curMaxBytes = +fs.readFileSync(maxArcFile).toString(),
+    c = require('chalk'),
+    clear = require('cli-clear'),
+    child = require('child_process'),
+    os = require('os');
 
 
 
 
-rangePrompt('Adjust the ZFS ARC Size', {
-	min: 0, max: 10, value: 2, step: .1, unit: 'kg'
-})
- .on('abort', (item) => console.log('You aborted, having chosen', item, 'kg'))
- .on('submit', (item) => console.log('You chose', item, 'kg'))
+var freeM = child.execSync('free -m').toString();
+clear();
+console.log('free -m : \n' + c.green(freeM));
+var msg = c.cyan.bgBlack('Current ZFS ARC Max is ') + c.white.bgBlack(prettyBytes(curMaxBytes));
+var promptMsg = 'Select new ARC Size: ';
+var unit = 'MB';
+var min = 256;
+var max = +os.totalmem() / 1024 / 1024;
+//var step = 128;
+var step = parseInt(max/8);
+
+console.log('max=', max);
+//process.exit();
+console.log(c.cyan.bgWhite(msg) + '\n');
+
+
+var curValue = parseInt(curMaxBytes / 1024 / 1024);
+curValue = parseInt(curValue / step) * step;
+if (curValue < min)
+    curValue = min;
+if (curValue > max)
+    curValue = max;
+
+var rP = {
+    min: min,
+    max: max,
+    value: curValue,
+    step: step,
+    unit: unit
+};
+console.log(pj.render(rP)+'\n\n');
+
+rangePrompt(promptMsg, rP)
+    .on('abort', (item) => console.log('You aborted, having chosen', item, 'kg'))
+    .on('submit', (item) => console.log('You chose', item, 'kg'))
